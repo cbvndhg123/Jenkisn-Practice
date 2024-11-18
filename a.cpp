f1398c1bffd7179553e3583f0953bb88016968f1
cbvndhg123 <xpqr03855@gmail.com>
2024-11-18 15:37:58 +0530

    added parametes

---
+++ a.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int>arr={1,2,3,4,5}
+
+vector<int>result;
+
+for(int i=k;i<arr.size();i++)
+    result.push_back(arr[i]);
+
+for(int i=0;i<k;i++)
+    result.push_back(arr[i]);
